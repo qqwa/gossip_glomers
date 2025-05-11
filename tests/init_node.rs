70ce31538f9f0cb55e5da8edea71f12a01fee5a5
Benjamin Bäumler <benjamin.baeumler@qqwa.de>
2025-05-11 17:27:20 +0000

    refactor tests

--- tests/init_node.rs
+++
@@ -1,9 +0,0 @@
-use common::TestServer;
-mod common;
-
-#[test]
-fn init_message() {
-    TestServer::new()
-        .send_str( r#"{"id":0,"src":"c0","dest":"n0","body":{"type":"init","node_id":"n0","node_ids":["n0"],"msg_id":1}}"#)
-        .expect_raw_message(r#"{"src":"n0","dest":"c0","body":{"type":"init_ok","in_reply_to":1}}"#);
-}
--- tests/server.rs
+++ tests/server.rs
@@ -2,6 +2,13 @@ use common::TestServer;
 mod common;
 
 #[test]
+fn init_message() {
+    TestServer::new()
+        .send_str( r#"{"id":0,"src":"c0","dest":"n0","body":{"type":"init","node_id":"n0","node_ids":["n0"],"msg_id":1}}"#)
+        .expect_raw_message(r#"{"src":"n0","dest":"c0","body":{"type":"init_ok","in_reply_to":1}}"#);
+}
+
+#[test]
 fn echo_message() {
     TestServer::new()
         .send_str( r#"{"src":"c1","dest":"n1","body":{"type":"echo","msg_id":1,"echo":"Please echo 35"}}"#)
