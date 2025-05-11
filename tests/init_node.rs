e43af788b96c4d9b88567c3c459751c960464414
Benjamin Bäumler <benjamin.baeumler@qqwa.de>
2025-05-11 15:10:51 +0000

    remove initial acceptance test that didn't use testing infrastructure

--- tests/init_node.rs
+++ tests/init_node.rs
@@ -1,20 +1,5 @@
-use std::io::BufRead;
-
 use common::TestServer;
-use gossip_glomers::Server;
 mod common;
-
-#[test]
-fn init() {
-    let msg = r#"{"id":0,"src":"c0","dest":"n0","body":{"type":"init","node_id":"n0","node_ids":["n0"],"msg_id":1}}"#;
-    let msg = msg.to_string() + "\n";
-    let mut server = Server::new();
-    let mut output = Vec::new();
-    server.serve(&mut msg.as_bytes(), &mut output, &mut Vec::new());
-    let got = output.lines().map(|l| l.unwrap()).collect::<Vec<String>>();
-    let want = vec![r#"{"src":"n0","dest":"c0","body":{"type":"init_ok","in_reply_to":1}}"#];
-    assert_eq!(got, want);
-}
 
 #[test]
 fn init_message() {
